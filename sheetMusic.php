eb8b1f9033ed463df8789278bfc522107c36d1af
Aaron Martin <sbbeachvball@gmail.com>
2015-10-22 08:31:18 -0700

    Adding in the code that was the original inspiration for this project

---
+++ sheetMusic.php
@@ -0,0 +1,10 @@
+<html><head><style>
+hr { padding: 0; margin: 6px; margin-bottom: 16px; border: 1px solid black; }
+br { display: block; line-height: 40px; }
+</style></head></body>
+<?php  
+    for($i=0;$i<12;$i++){
+        print "<br>";  for($j=0;$j<5;$j++)  print "<hr>";
+   }
+?>
+</body></html>
